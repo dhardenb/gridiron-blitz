5fd3a55e0536ef263ea5c5a548acbb8120db6fcb
David Hardenbrook <david.hardenbrook@kohls.com>
2020-10-10 21:18:06 -0500

    Adding testing

--- gridiron.js
+++ gridiron.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -13,25 +13,21 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortune.getFortune() } )
-})
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-  })
-  
-// custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.notFound)
 
-app.listen(port, () => {
-    console.log("Gridiron listening on http://locahost: " + port)
-})
+// custom 500 page
+app.use(handlers.serverError)
+
+if(require.main === module) {
+    app.listen(port, () => {
+      console.log( `Express started on http://localhost:${port}` +
+        '; press Ctrl-C to terminate.' )
+    })
+  } else {
+    module.exports = app
+  }
